0379a716838ceaf793e362dd8580921f88db7a0b
Projetos <joaomarcos_cp@hotmail.com>
2022-12-30 14:58:27 -0300

    first commit

---
+++ src/server/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { CidadesController } from './../controllers';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+
+	return res.send('Ola, dev!');
+});
+
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
+
+
+export { router };
