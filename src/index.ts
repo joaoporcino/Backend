9233f980888f2a73c59e17b9e1001a1b753430e2
Projetos <joaomarcos_cp@hotmail.com>
2023-01-02 07:58:38 -0300

    corrigido o codigo para produção

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 3333, () => {
-	console.log(`App rodando na porta ${process.env.PORT || 3333}`);
-});
+const StartServer = () => {
+	server.listen(process.env.PORT || 3333, () => {
+		console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+	});
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+
+	Knex.migrate.latest()
+		.then(() => {
+			StartServer();
+		})
+		.catch(console.log);
+
+} else {
+	StartServer();
+}
